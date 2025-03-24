4c8856a129b6c89ba071b7337f46ceae6e20b8d8
Kshitij Khanal <kshitijkhanal2005@hotmail.com>
2025-03-24 09:01:19 +0530

    Update practical1.cpp

--- practical1.cpp
+++ practical1.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 double computeSeriesSum(int n) {
     double sum = 0.0;
-    int sign = 1;  // To alternate between + and -
+    int sign = 1;  
     
     for (int i = 1; i <= n; i++) {
-        // Add term: sign * 1/(i^i)
+        
         sum += sign * (1.0 / pow(i, i));
-        sign = -sign;  // Flip sign for next term
+        sign = -sign;  
     }
     
     return sum;
@@ -20,26 +20,26 @@ double computeSeriesSum(int n) {
 int main(int argc, char* argv[]) {
     int n;
     
-    // Check if command line argument is provided
+    
     if (argc > 1) {
-        n = atoi(argv[1]);  // Convert string argument to integer
+        n = atoi(argv[1]);  
         if (n <= 0) {
             cout << "Please enter a positive number of terms." << endl;
             return 1;
         }
     } else {
-        // Prompt user for input if no command line argument
+        
         cout << "Enter the number of terms (n): ";
         cin >> n;
         
-        // Validate input
+        
         if (n <= 0) {
             cout << "Please enter a positive number of terms." << endl;
             return 1;
         }
     }
     
-    // Compute and display the result
+    
     double result = computeSeriesSum(n);
     cout << "Sum of the first " << n << " terms of the series is: " << result << endl;
     
